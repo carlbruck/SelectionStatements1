b0bdb1a2f4b3bd3044acfa3a03b9dc19cd95a5d3
carlbruck <carl.bruckner@gmail.com>
2021-01-26 15:44:52 -0500

    first commit

---
+++ Selection Statements 1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Selection_Statements_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string selection = "option 1";
+
+
+            if ("option 1 == option)
+            {
+                Console.WriteLine("Great! You chose Option 1!");
+
+
+                else if ("option 2 == choice") ;
+
+            }
+                { 
+                    Console.WriteLine("You chose option 2!");
+                }
+
+            else
+            {
+            Console.WriteLine(You chose a different option");")
+            }
+
+            string department = "Groceries";
+
+            switch(department)
+
+            {
+                case "groceries":
+                    Console.WriteLine("You need to look on aisle 1");
+                    break;
+
+                case "electronics":
+                    Console.WriteLine("You need to look on aisle 5");
+
+                default:
+                    Console.WriteLine("No departments exist for that option");
+
+
+
+
+
+            }
+
+        }
+    }
+}
